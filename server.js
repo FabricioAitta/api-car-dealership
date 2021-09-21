c7b6584f3c4209547eac1faf78cfaee59b596cbf
FabricioAitta <aittafabricio@gmail.com>
2021-09-21 18:20:55 -0300

    cors install

--- server.js
+++ server.js
@@ -4,9 +4,12 @@ const express = require('express')
 const app = express()
 const routes = require('./routes')
 const PORT = process.env.PORT || 3001
+const cors = require('cors')
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json({limit: '50mb'}))
+
+app.use(cors())
 
 app.use("/api", routes);
 
