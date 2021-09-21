07ddf3947900b4d3b3c38e093bdec9654c9c4d9f
FabricioAitta <aittafabricio@gmail.com>
2021-09-21 19:38:18 -0300

    register y login listos

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -3,19 +3,31 @@ const User = require('../models/user')
 
 const authController = {
     register: (req, res, next) => {
-        User.find()
-        .then((cars) => res.send(cars).status(200))
+        const [name, email, password] = req.body.user
+        User.find({email: email})
+        .then((user) => {
+            if(!user){
+                return User.create({
+                    name: name,
+                    email: email,
+                    password: password
+                })
+            }
+        })
+        .then((user) => res.send(user).status(200))
         .catch((err) => res.send(err).status(400))
     },
     login: (req, res, next) => {
-        User.findById({_id: req.params.id})
-        .then((car) => res.send(car).status(200))
+        const [name, email, password] = req.body.user
+        User.find({email: email})
+        .then((user) => res.send(user).status(200))
         .catch((err) => res.send(err).status(400))
     },
     update: (req, res, next) => {
-        res.send("ruta no funcional")
-        // User.create(req.body)
-        // .then((car) => res.send(car).status(200))
+        res.send("hola")
+        // const [name, email, password] = req.body.user
+        // User.findOneAndUpdate({email: email})
+        // .then((user) => res.send(user).status(400))
         // .catch((err) => res.send(err).status(400))
     }
 }
